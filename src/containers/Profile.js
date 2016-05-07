9aa6bb4a77f99d9e024c6b3b38d93256fdd5eda8
tinygipxy <hoai0906@gmail.com>
2016-05-07 16:29:45 +0800

    Add sample information on profile page for myAds

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -13,7 +13,7 @@ import * as globalActions from '../reducers/global/globalActions';
 import {Map} from 'immutable';
 
 
-import React, {Text, View, Component, StyleSheet} from 'react-native'
+import React, {Text, View, Component, StyleSheet, ListView, Image} from 'react-native'
 
 import Button from 'react-native-button';
 import {Actions} from 'react-native-router-flux';
@@ -58,7 +58,11 @@ class Profile extends Component {
     constructor() {
         super();
         this.state = {
-            myAds:[]
+            myAds:[],
+
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2
+            })
         };
     }
     componentDidMount() {
@@ -90,32 +94,51 @@ class Profile extends Component {
     onTestSync() {
         dbService.getAllAds()
             .then((res) => {
-                console.log(res.rows);
+                console.log(res);
+
+                var listAds = res.map((one) => {
+                    return one.value;
+                });
 
                 this.setState({
-                    myAds: res.rows
+                    myAds: listAds,
+                    dataSource: this.state.dataSource.cloneWithRows(listAds)
                 })
             });
+    }
+
+    _renderAds(ads) {
+        //var ads = ads.doc;
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: ads.image.cover}}
+                    style={styles.thumbnail}/>
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{ads.adsID} - {ads.dangBoi.name}</Text>
+                    <Text style={styles.year}>{ads.place.diaChi}</Text>
+
+                </View>
+            </View>
+        );
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.text}>........Profile screen........</Text>
-                <Text style={styles.text}>
-                    Very long text to test new font that we will use for all app
-                    Spacious, Move In Condition, 2 bedroom corner apartment that has 2 exposures. There are
-                    plenty of closets. Walk to train, bus, shopping, dining, beach, marina and NewRoc Center.
-                    Exercise room and laundry in building
-                </Text>
-                <Text style={styles.text}>Welcome: {this.props.global.currentUser.userID}</Text>
 
-                <Button onPress={this.onTestSync.bind(this)}>TestSync</Button>
-                <Button onPress={this.onAutoSync.bind(this)}>Auto Sync each 100ms</Button>
-                <Button onPress={this.onStopAutoSync.bind(this)}>Stop Sync each 100ms</Button>
+                <Button style={styles.btn} onPress={this.onTestSync.bind(this)}>TestSync</Button>
+                <Button style={styles.btn} onPress={this.onAutoSync.bind(this)}>Auto Sync each 100ms</Button>
+                <Button style={styles.btn} onPress={this.onStopAutoSync.bind(this)}>Stop Sync each 100ms</Button>
 
 
                 <Text style={styles.text}>Number of my ads: {this.state.myAds ? this.state.myAds.length : 0}</Text>
+
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this._renderAds}
+                    style={styles.listView}/>
             </View>
         )
     }
@@ -126,11 +149,42 @@ export default connect(mapStateToProps, mapDispatchToProps)(Profile);
 
 
 var styles = StyleSheet.create({
-    container :{
-        flex: 1
+    container: {
+        top: 60,
+        flex: 1,
+        //flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
     },
+
     text : {
         fontFamily: gui.fontFamily,
         fontSize: gui.fontSize
+    },
+    btn: {
+        margin: 10
+    },
+
+    thumbnail: {
+        width: 120,
+        height: 60
+    },
+    rightContainer: {
+        flex: 1
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center'
+    },
+    year: {
+        textAlign: 'center'
+    },
+    listView: {
+        paddingTop: 0,
+        backgroundColor: '#F5FCFF',
+        flex : 1,
+        marginBottom: 140
     }
 });
