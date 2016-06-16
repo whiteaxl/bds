e25430ff276c341289575423d0c0881718a8c9bd
hainguyen1981 <nguyenbhcse@gmail.com>
2016-06-16 08:17:37 +0700

    Add UploadApi.
    Add UploadApi.

---
+++ src/lib/UploadApi.js
@@ -0,0 +1,49 @@
+// Api.js
+
+import ApiUtils from './ApiUtils';
+import RangeUtils from "../lib/RangeUtils"
+
+import DanhMuc from "../assets/DanhMuc"
+import cfg from "../cfg";
+
+var rootUrl = `http://${cfg.server}:5000/api`;
+var uploadUrl = rootUrl + "/upload";
+
+'use strict';
+
+var React = require('react-native');
+var FileUpload = require('NativeModules').FileUpload;
+
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+} = React;
+
+var Api = {
+  onUpload: function(filename, filepath, successCallback) {
+      var obj = {
+          uploadUrl: uploadUrl,
+          method: 'POST', // default 'POST',support 'POST' and 'PUT'
+          headers: {
+              'Accept': 'application/json'
+          },
+          files: [
+              {
+                  filename: filename, // require, file name
+                  filepath: filepath, // require, file absoluete path
+                  filetype: 'image/jpeg' // options, if none, will get mimetype from `filepath` extension
+              }
+          ]
+      };
+      return FileUpload.upload(obj, function(err, result) {
+          //console.log('upload: ' + uploadUrl, err, result);
+          if (!err) {
+              successCallback(result);
+          }
+      });
+  }
+};
+
+export { Api as default };
